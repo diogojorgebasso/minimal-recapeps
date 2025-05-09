32bb53ef03c1f9ce3b50e2289b204a8a5484e65f
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-05-09 13:21:24 +0200

    WIP : remove two App

--- src/clientApp.js
+++ src/clientApp.js
@@ -1,16 +1,11 @@
 'use client';
 
 import {
-    initializeApp, getApp, getApps
+    initializeApp
 } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Initialize Firebase
-const app = getApps().length === 0
-    ? initializeApp()
-    : getApps()[0];
-console.log("app instance", app)
-export const firebaseApp = initializeApp(app);
+export const firebaseApp = initializeApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
